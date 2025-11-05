ce96bdf6b1f9fc064e5b238c5a281283733aa745
Emanuel Mena Araya <emena9215@gmail.com>
2025-11-04 19:48:20 -0600

    added typechecking

--- vigia/vite.config.ts
+++ vigia/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true, // corre tsc en paralelo a Vite
+      // opcional: eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+    }),
+  ],
   server: {
         host: true, // or '0.0.0.0'
       },
